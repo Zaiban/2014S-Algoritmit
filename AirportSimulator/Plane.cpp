d0c284ca721fe52f6e6a87bbd0369cfb1485e988
Zaiban <esaparkkila@gmail.com>
2014-11-14 08:05:39 +0200

    AirportSimulator stubs ready

---
+++ AirportSimulator/Plane.cpp
@@ -0,0 +1,30 @@
+#include "Plane.h"
+
+Plane::Plane()
+{
+	//TO-DO
+}
+Plane::Plane(int flt, int time, Plane_status status)
+{
+	//TO-DO
+}
+Plane::~Plane()
+{
+	//TO-DO
+}
+void Plane::refuse() const
+{
+	//TO-DO
+}
+void Plane::land(int time) const
+{
+	//TO-DO
+}
+void Plane::fly(int time) const
+{
+	//TO-DO
+}
+int Plane::started() const
+{
+	//TO-DO
+}
---
+++ AirportSimulator/Runway.cpp
@@ -0,0 +1,26 @@
+#include "Runway.h"
+
+Runway::Runway(int limit)
+{
+	//TO-DO
+}
+Runway::~Runway()
+{
+	//TO-DO
+}
+Error_code Runway::can_land(const Plane &current)
+{
+	//TO-DO
+}
+Error_code Runway::can_depart(const Plane &current)
+{
+	//TO-DO
+}
+Runway_activity Runway::activity(int time, Plane &moving)
+{
+	//TO-DO
+}
+void Runway::shut_down(int time) const
+{
+	//TO-DO
+}
